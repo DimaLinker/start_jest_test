7af975a0bf48bca2cd02febecb9c59a492473fab
Dima Linker <daniel.linker@mailbox.org>
2024-03-28 15:59:04 +0100

    multiply test

--- multiply.test.js
+++ multiply.test.js
@@ -1,8 +1,25 @@
 import {multiply} from "./multiply";
+import expect from "expect";
+
+describe('multiply', () => {
+    it('should multiply positive numbers', () => {
+        const res = multiply(3, 2)
+
+        expect(res).toBe(6)
+    });
+
+    it('should multiply negative numbers', () => {
+        const res = multiply(-3, -2)
+
+        expect(res).toBe(6)
+    });
+
+    it('should multiply positive and negative numbers', () => {
+        const res = multiply(-3, 2)
+
+        expect(res).toBe(6)
+    })
 
 
-test('multiply ', () => {
-    const res = multiply(3, 2)
-
-    expect(res).toBe(6)
 })
+
